e6bbc34759ae494f8ec0b32ff77ab508811e2fdd
juan <juan.ramirez.sk@gmail.com>
2019-07-14 10:54:38 -0400

    funcionalidad botones busqueda

--- FrontAngular/v1/src/app/componentes/busqueda-id/busqueda-id.component.ts
+++ FrontAngular/v1/src/app/componentes/busqueda-id/busqueda-id.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { RsServiceService } from 'src/app/Services/rs-service.service';
 import { ReclamoSugerencia } from 'src/app/Modelo/ReclamoSugerencia';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-busqueda-id',
   templateUrl: './busqueda-id.component.html',
@@ -9,13 +11,31 @@ import { ReclamoSugerencia } from 'src/app/Modelo/ReclamoSugerencia';
 })
 export class BusquedaIdComponent implements OnInit {
 
-  constructor(private rsService:RsServiceService) { }
+  constructor(private rsService:RsServiceService,private router:Router) { }
   rs:ReclamoSugerencia;
+  idBusqueda:number;
+  mostrarPerfil:boolean=false;
+  noMostrarPerfil:boolean;
   ngOnInit() {
+    this.noMostrarPerfil=!this.mostrarPerfil;
+    console.log(localStorage.getItem("Email"))
+    if(!(localStorage.getItem("Email")=="anonimo")){
+      this.mostrarPerfil=true;
+    }
     this.rsService.getReclamo(+(localStorage.getItem("idBusqueda"))).subscribe(params =>{
       this.rs=params;
     });
 
   }
-
+  cerrarSesion(){
+    localStorage.setItem("Email", "anonimo");
+    this.router.navigate(["home"])
+  }
+  buscarPorId(){
+    localStorage.setItem("idBusqueda",""+this.idBusqueda);
+    this.rsService.getReclamo(+(localStorage.getItem("idBusqueda"))).subscribe(params =>{
+      this.rs=params;
+    });
+    
+  }
 }
