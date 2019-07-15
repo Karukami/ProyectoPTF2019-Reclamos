9928cf8effcc59791e6b7a892ed1179ec3dfb879
juan <juan.ramirez.sk@gmail.com>
2019-07-15 10:31:24 -0400

    agregado metodo crear trabajador

--- FrontAngular/v1/src/app/Services/trabajador-service.service.ts
+++ FrontAngular/v1/src/app/Services/trabajador-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Empresa } from '../Modelo/Empresa';
 import { ReclamoSugerencia } from '../Modelo/ReclamoSugerencia';
+import { Trabajador } from '../Modelo/trabajador';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,6 +13,11 @@ export class TrabajadorServiceService {
   obtenerReclamosSugerencias(){
     return this.http.get<ReclamoSugerencia>(this.url);
   }
-  
+  //crearTrabajador():Trabajador ->vacio ->realiza una pericion post al servicor de back-end
+  //envia atravex de metodo post el objeto Trabajador 
+  //el cual sera agregado a la db desde el back
+  crearTrabajador(trabajador:Trabajador){
+    return this.http.post<Trabajador>(this.url,trabajador);
+  }
 
 }
