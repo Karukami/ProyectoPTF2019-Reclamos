caaf1ce42c00a08675b8db5027fbe4f6f7d32f99
suZanahoria287 <suZanahoria287@DESKTOP-OUSO92R>
2019-07-15 11:04:07 -0400

    mas metodos en Trabajador

--- proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/repositories/TrabajadorRepositorio.java
+++ proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/repositories/TrabajadorRepositorio.java
@@ -1,10 +1,14 @@
 package com.example.proyectoV1.repositories;
+import java.util.List;
+
 import org.springframework.data.repository.Repository;
 import com.example.proyectoV1.entities.Trabajador;
+
 
 public interface TrabajadorRepositorio extends Repository<Trabajador,Integer>{
 	Trabajador save(Trabajador t);
 	Trabajador findOne(int idTrabajador);
-	Trabajador findAll();
+	List<Trabajador> findAll();
+	Trabajador delete(Trabajador t);
 	
 }
