0d48bde372272667705e544039182879d3f918d3
juan <juan.ramirez.sk@gmail.com>
2019-07-15 09:50:31 -0400

    creacion y documentacion metodo crearEmpresa

--- FrontAngular/v1/src/app/Services/empresa-service.service.ts
+++ FrontAngular/v1/src/app/Services/empresa-service.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Empresa } from '../Modelo/Empresa';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpresaServiceService {
+  constructor(private http:HttpClient) { }
+  url='http://localhost:8080/Empresa'
 
-  constructor() { }
+  //crearEmpresa:Empresa->vacio->realiza una pericion post al servicor de back-end
+  //envia atravez del metodo post el objeto empresa 
+  //el cual en le back-end sera agregado a la db
+  crearEmpresa(empresa:Empresa){
+  	return this.http.post<Empresa>(this.url,empresa);
+  }
   
+
+  
+
 }
