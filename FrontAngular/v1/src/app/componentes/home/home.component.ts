bfca63b4c111eeb1bdfdfb0d02b30cdff5733da2
juan <juan.ramirez.sk@gmail.com>
2019-07-15 13:18:10 -0400

    documentacion componente home

--- FrontAngular/v1/src/app/componentes/home/home.component.ts
+++ FrontAngular/v1/src/app/componentes/home/home.component.ts
@@ -20,17 +20,24 @@ export class HomeComponent implements OnInit {
       this.router.navigate(["perfil"]);
     }
   }
+  //registrar():vacio->vacio
+  //redirige al componente registrar
   registrar(){
     this.router.navigate(['registrar']);
   }
+  //login():vacio-> vacio
+  //redirige al componente login
   login(){
     this.router.navigate(['login']);
   }
-
+  //homeEmpresa():vacio->vacio
+  //redirige al componente home_empresa
   homeEmpresa(){
     this.router.navigate(['home_empresa']);
   }
-
+  //buscarPorId(): vacio -> vacio
+  //guarda el id de busqueda idbusqueda y 
+  //redirige al componente buscar_id
   buscarPorId(){
     localStorage.setItem("idBusqueda",""+this.idBusqueda);
     this.router.navigate(['buscar_id']);
