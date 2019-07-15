b91ede33401871f9a9a00505b988c4ac33daded1
juan <juan.ramirez.sk@gmail.com>
2019-07-15 09:53:46 -0400

    objeto trabajador

---
+++ FrontAngular/v1/src/app/Modelo/trabajador.ts
@@ -0,0 +1,18 @@
+export class trabajador{
+    idTrabajador:number;
+    nombretrabajador:string;
+    apellidoTrabajador:string;
+    tipoTrabajador:string;
+    passtrabajador:string;
+  
+    
+    constructor(){
+
+      this.idTrabajador=0;
+      this.nombretrabajador="";
+      this.tipoTrabajador="";
+      this.passtrabajador="";
+    }
+  
+    
+}
