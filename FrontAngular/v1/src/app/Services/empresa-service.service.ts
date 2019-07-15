01712f9697099ea1c851bf85b5c0dc1adccdb013
juan <juan.ramirez.sk@gmail.com>
2019-07-15 10:05:46 -0400

    creacion de servicio trabajador y documentacion

--- FrontAngular/v1/src/app/Services/empresa-service.service.ts
+++ FrontAngular/v1/src/app/Services/empresa-service.service.ts
@@ -12,12 +12,14 @@ export class EmpresaServiceService {
 
   //crearEmpresa:Empresa->vacio->realiza una pericion post al servicor de back-end
   //envia atravez del metodo post el objeto empresa 
-  //el cual en le back-end sera agregado a la db
+  //el cual  sera agregado a la db desde el back
   crearEmpresa(empresa:Empresa){
   	return this.http.post<Empresa>(this.url,empresa);
   }
   
-
+  //crearTrabajador():Trabajador ->vacio ->realiza una pericion post al servicor de back-end
+  //envia atravex de metodo post el objeto Trabajador 
+  //el cual sera agregado a la db desde el back
   crearTrabajador(trabajador:trabajador){
     return this.http.post<trabajador>(this.url,trabajador);
   }
