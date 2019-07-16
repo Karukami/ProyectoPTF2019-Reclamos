d9c32955411bfa1a7d514f6ad55cc6f0df777f88
suZanahoria287 <zerimar287@gmail.com>
2019-07-16 19:10:52 -0400

    Metodo RS

--- proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/services/ReclamoSugerenciaServiceImp.java
+++ proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/services/ReclamoSugerenciaServiceImp.java
@@ -1,13 +1,9 @@
 package com.example.proyectoV1.services;
-
-
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.proyectoV1.entities.ReclamoSugerencia;
 import com.example.proyectoV1.repositories.ReclamoSugerenciaRepositorio;
-
 @Service
 public  class ReclamoSugerenciaServiceImp implements ReclamoSugerenciaService{
 	
@@ -24,6 +20,10 @@ public  class ReclamoSugerenciaServiceImp implements ReclamoSugerenciaService{
 	@Override
 	public ReclamoSugerencia edit(ReclamoSugerencia x) {
 		return repositorio.save(x);
+	}
+	@Override
+	public List<ReclamoSugerencia> rsByusuarioReclamoSugerencia(int usuarioReclamoSugerencia) {
+		return repositorio.findByusuarioReclamoSugerencia(usuarioReclamoSugerencia);
 	}
 	
 }
