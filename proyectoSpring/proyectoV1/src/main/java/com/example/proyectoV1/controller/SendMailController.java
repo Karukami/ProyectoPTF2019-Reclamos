8935b1886bcc4a4f1b457a970ad8766930848c06
suZanahoria287 <zerimar287@gmail.com>
2019-07-17 10:57:49 -0400

    Metodo EnviarEmail

--- proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/controller/SendMailController.java
+++ proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/controller/SendMailController.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.proyectoV1.services.SendMailService;
 @CrossOrigin(origins="http://localhost:4200",maxAge=3600)
 @RestController
@@ -19,15 +17,18 @@ import com.example.proyectoV1.services.SendMailService;
 public class SendMailController {
 	@Autowired
 	private SendMailService sendMailService;
+	private ReclamoSugerenciaControlador controllRS;
+	private ControladorUsuario user;
 	
 	@GetMapping("/sendMail/mail")
 	 public String index() {
 		return "send_mail_view";
 	}
-	@PostMapping("/sendMail")
-	public String sendMail(@RequestParam("name") String name, @RequestParam("mail") String mail, @RequestParam("subject") String subject, @RequestParam("body")String body) {
-		String message  =body + "\n\n Datos de contacto:" + "\nNombre: " + name + "\nEmail: " + mail;
-		sendMailService.sendMail("zerimar287@gmail.com", "zerimar287@gmai.com", subject, message);
+	@RequestMapping (value="/{send}", method=RequestMethod.GET)
+	public String sendMail(int userRut ) {
+		String message = "Estimad@ " + user.listarRutUsuario(userRut).getNombreUsuario() + " : \n\n" + "Tu reclamo ha sido ingresado con exito! \n\n" 
+		+ "Estate atento a la respuesta de tu reclamo con el numero de ID " + controllRS.rsByUser(userRut) + "\n\n\nGracias por utilizar nuestra plataforma!";
+		sendMailService.sendMail(user.listarRutUsuario(userRut).getEmailUsuario(),"Reclamo ID: " + controllRS.rsByUser(userRut),message);
 		return "send_mail_view";
 	}
 }
