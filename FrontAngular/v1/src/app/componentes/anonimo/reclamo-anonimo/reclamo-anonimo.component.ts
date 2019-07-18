66a788885d18153e16951e102b7d0abbff2b69f6
juan <juan.ramirez.sk@gmail.com>
2019-07-18 09:12:51 -0400

    realizar reclamo anonimo ok

--- FrontAngular/v1/src/app/componentes/anonimo/reclamo-anonimo/reclamo-anonimo.component.ts
+++ FrontAngular/v1/src/app/componentes/anonimo/reclamo-anonimo/reclamo-anonimo.component.ts
@@ -3,6 +3,7 @@ import { ReclamoSugerencia } from 'src/app/Modelo/ReclamoSugerencia';
 import { Router } from '@angular/router';
 import { RsServiceService } from 'src/app/Services/rs-service.service';
 import { EmpresaServiceService } from 'src/app/Services/empresa-service.service';
+import { Empresa } from 'src/app/Modelo/Empresa';
 
 @Component({
   selector: 'app-reclamo-anonimo',
@@ -28,26 +29,57 @@ export class ReclamoAnonimoComponent implements OnInit {
     
     });
     }
-  
+   
   realizarReclamoSugerencia() {
-    this.rs.tipo="Reclamo";
-    
-    this.rs.idReclamoSugerencia=0;
-    this.rs.idEmpleado=0;
-    this.rs.estado="en proseso";
-    this.rs.usuarioReclamoSugerencia=Number(localStorage.getItem('anonimo'));
-    this.rs.respuestaRS="aun no hay respuesta";
-    this.rs.fechaReclamoSugerencia=new Date();
-    localStorage.setItem("tituloRS",this.rs.tituloRS);
-    localStorage.setItem("empresa",""+this.rs.idEmpresa);
-    localStorage.setItem("idRS",""+this.rs.idReclamoSugerencia);
-    localStorage.setItem("fecha",this.rs.fechaReclamoSugerencia.toString())
-    localStorage.setItem("detalleRS",this.rs.detalleReclamoSugerencia);
-    localStorage.setItem("tipo",this.rs.tipo);
-
-    this.serviceRS.crearReclamo(this.rs).subscribe(data =>{this.rs= data});
-
-    this.router.navigate(["anonimo/reclamo_sugerencia_anonimo_enviado"]);
+    try {
+      this.servicioEmpresa.idEmpresa(this.empresa).subscribe(data=>{
+        let emp:Empresa=data;
+        
+        this.rs.idEmpresa=emp.rutEmpresa;
+        console.log(this.rs.idEmpresa);
+        this.rs.tipo="Reclamo";
+        
+        this.rs.idReclamoSugerencia=0;
+        this.rs.idEmpleado=0;
+        this.rs.estado="en proceso";
+      
+        this.rs.respuestaRS="aun no tiene respuesta";
+        
+        this.rs.fechaReclamoSugerencia=new Date();
+        
+        localStorage.setItem("tituloRS",this.rs.tituloRS);
+        localStorage.setItem("empresa",this.rs.idEmpresa+"");
+        localStorage.setItem("idRS",""+this.rs.idReclamoSugerencia);
+        localStorage.setItem("fecha",this.rs.fechaReclamoSugerencia.toString());
+        localStorage.setItem("detalleRS",this.rs.detalleReclamoSugerencia);
+        localStorage.setItem("tipo",this.rs.tipo);
+        this.serviceRS.crearReclamo(this.rs).subscribe(data =>{this.rs= data});
+  
+        try {
+          this.serviceRS.getLastReclamoUsuario(this.rs.usuarioReclamoSugerencia).subscribe(data=>{
+            let rs:ReclamoSugerencia=data;
+            
+            localStorage.setItem("idRS",""+rs.idReclamoSugerencia);
+            console.log(localStorage.getItem("idRS"));
+            //alert("reclamo generado enviado con exito ");
+            this.router.navigate(["rs_enviado"]);
+          });
+        } catch (error) {
+          this.serviceRS.getLastReclamo().subscribe(data=>{
+            let rs:ReclamoSugerencia=data;
+            
+            localStorage.setItem("idRS",""+rs.idReclamoSugerencia);
+            console.log(localStorage.getItem("idRS"));
+            //alert("reclamo generado enviado con exito ");
+            this.router.navigate(["rs_enviado"]);
+          });
+        }
+       
+      })
+      
+    } catch (error) {
+    console.log("error");
+    }
   }
 
   buscarPorId(){
