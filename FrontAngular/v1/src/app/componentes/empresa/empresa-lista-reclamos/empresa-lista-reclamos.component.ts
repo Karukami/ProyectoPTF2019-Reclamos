7646295ff7f3bb4909ec850d989fa4d6b1b4e9a5
juan <juan.ramirez.sk@gmail.com>
2019-07-18 10:53:48 -0400

    lilsta auto generable de reclamos perfil empresa

--- FrontAngular/v1/src/app/componentes/empresa/empresa-lista-reclamos/empresa-lista-reclamos.component.ts
+++ FrontAngular/v1/src/app/componentes/empresa/empresa-lista-reclamos/empresa-lista-reclamos.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ReclamoSugerencia } from 'src/app/Modelo/ReclamoSugerencia';
+import { dashCaseToCamelCase } from '@angular/compiler/src/util';
+import { DeprecatedDatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-empresa-lista-reclamos',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpresaListaReclamosComponent implements OnInit {
 
+  reclamos:ReclamoSugerencia[]=[];
+  colores:string[]=[];
+  
   constructor() { }
 
   ngOnInit() {
+    for(let a =0;a<10;a++){
+
+      let color:string;
+
+      let rs:ReclamoSugerencia=new ReclamoSugerencia();
+      rs.detalleReclamoSugerencia=""+a;
+      rs.fechaReclamoSugerencia=new Date();
+      rs.idEmpleado=a;
+      rs.detalleReclamoSugerencia="adsfdgtrhseñfldvfkblgxdt";
+      rs.tipo="Reclamo";
+      let dia = rs.fechaReclamoSugerencia.getDay
+      console.log("dia --"+dia);
+
+      let esteDia=new Date();
+      
+      if (dia<=esteDia.getDay){
+
+       color="rgba(255, 255, 25, 0.9)";
+      }
+      else{
+        color="rgba(255, 255, 255, 0.9)";
+
+      }
+      console.log(color);
+      this.reclamos.push(rs);
+      this.colores.push(color);
+    }
   }
 
 }
