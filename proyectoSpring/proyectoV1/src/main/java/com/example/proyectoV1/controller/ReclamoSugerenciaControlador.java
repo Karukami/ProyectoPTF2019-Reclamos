2c968ab3d06a24743fab0495a9828bb82f52c04d
suZanahoria287 <zerimar287@gmail.com>
2019-07-22 15:45:21 -0400

    Lista de trabajadores por id empresa

--- proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/controller/ReclamoSugerenciaControlador.java
+++ proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/controller/ReclamoSugerenciaControlador.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
