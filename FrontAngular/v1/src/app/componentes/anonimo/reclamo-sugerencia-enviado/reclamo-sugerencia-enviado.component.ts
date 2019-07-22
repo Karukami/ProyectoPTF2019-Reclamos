5e5d45f734c169c23c5517552b2eaac3d96624e0
Yutaowo <yutaowo@gmail.com>
2019-07-22 10:16:57 -0400

    Editada vista RS Enviado

--- FrontAngular/v1/src/app/componentes/anonimo/reclamo-sugerencia-enviado/reclamo-sugerencia-enviado.component.ts
+++ FrontAngular/v1/src/app/componentes/anonimo/reclamo-sugerencia-enviado/reclamo-sugerencia-enviado.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { RsServiceService } from 'src/app/Services/rs-service.service';
+import { ReclamoSugerencia } from 'src/app/Modelo/ReclamoSugerencia';
 
 @Component({
   selector: 'app-reclamo-sugerencia-enviado',
@@ -7,12 +9,50 @@ import { Router } from '@angular/router';
   styleUrls: ['./reclamo-sugerencia-enviado.component.css']
 })
 export class ReclamoSugerenciaEnviadoComponent implements OnInit {
+  idBusqueda:number;
+  rs:ReclamoSugerencia;
 
-  constructor(private router:Router) { }
+  constructor(private router:Router, private service:RsServiceService) {
+   }
 
-  ngOnInit() {
+   ngOnInit() {
+    let nombre:string = localStorage.getItem('Email');
+    if(nombre!="anonimo"&& nombre!=null){
+      this.router.navigate(["perfil"]);
+    }
   }
   registrarse(){
     this.router.navigate(["registrar"]);
+  }
+   //registrar():vacio->vacio
+  //redirige al componente registrar
+  registrar(){
+    this.router.navigate(['registrar']);
+  }
+  //login():vacio-> vacio
+  //redirige al componente login
+  login(){
+    this.router.navigate(['login']);
+  }
+  //homeEmpresa():vacio->vacio
+  //redirige al componente home_empresa
+  homeEmpresa(){
+    this.router.navigate(['home_empresa']);
+  }
+  //buscarPorId(): vacio -> vacio
+  //guarda el id de busqueda idbusqueda y 
+  //redirige al componente buscar_id
+  buscarPorId(){
+    localStorage.setItem("idBusqueda",""+this.idBusqueda);
+    this.router.navigate(['buscar_id']);
+    
+  }
+  reclamo(){
+    if(localStorage.getItem("Email")==null||localStorage.getItem("Email")=="anonimo"){
+      this.router.navigate(["anonimo/realizar_reclamo"]);
+    }else{
+      this.router.navigate(["realizar_reclamo"]);
+
+    }
   }
 } 
