37e89d2a80399ec7dfce8600328ce26bfa929969
VanneSmile <vanessa.arriagada.b@gmail.com>
2019-07-23 11:28:59 -0400

    funca telefono

--- proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/entities/Usuario.java
+++ proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/entities/Usuario.java
@@ -63,11 +63,11 @@ public Date getFechaNacUsuario() {
 public void setFechaNacUsuario(Date fechaNacUsuario) {
 	this.fechaNacUsuario = fechaNacUsuario;
 }
-public int getFonousuario() {
+public int getFonoUsuario() {
 	return fonoUsuario;
 }
-public void setFonousuario(int fonousuario) {
-	this.fonoUsuario = fonousuario;
+public void setFonoUsuario(int fonoUsuario) {
+	this.fonoUsuario = fonoUsuario;
 }
 public String getGeneroUsuario() {
 	return generoUsuario;
