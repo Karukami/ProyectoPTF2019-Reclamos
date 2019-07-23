b9e5336867d24a9578b141bd478fbe7d12b5188a
Vanne <vanne@Vannes-MacBook-Pro.local>
2019-07-23 11:08:24 -0400

    validacion

--- FrontAngular/v1/src/app/componentes/registro-usuario/registro-usuario.component.ts
+++ FrontAngular/v1/src/app/componentes/registro-usuario/registro-usuario.component.ts
@@ -93,7 +93,6 @@ ngOnInit() {
 } 
 
   registro(){
-
       this.usuarioARegistrar.nombreUsuario=this.nombre;
       this.usuarioARegistrar.apellidoUsuario=this.apellido;
       this.usuarioARegistrar.rutUsuario=this.formatRut(this.rut);
@@ -101,7 +100,16 @@ ngOnInit() {
       this.usuarioARegistrar.fonoUsuario=this.formatFono(this.fono);
       this.usuarioARegistrar.generoUsuario="mono";
       this.usuarioARegistrar.emailUsuario=this.correo;
-      this.usuarioARegistrar.passUsuario=this.pass;
+      this.usuarioARegistrar.passUsuario=this.pass;  
+
+      console.log(this.nombre);
+      console.log(this.apellido);
+      console.log(this.rut);
+      console.log(this.fono);
+      console.log(this.correo);
+      console.log(this.pass);
+
+
       this.service.crearUsuarioPrueba(this.usuarioARegistrar).subscribe(data=>{
         alert("se agrego correctamente");
       })
